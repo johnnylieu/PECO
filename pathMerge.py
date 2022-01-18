a016d8c37279a495f6df35cf83a079a824ab9efe
Entropy <79678575+hdezbriant@users.noreply.github.com>
2022-01-18 02:35:03 -0800

    added error handling for NaNs

--- pathMerge.py
+++ pathMerge.py
@@ -15,24 +15,29 @@ def pathMerge(main_paths_file, helper_file, slotId):
             chosenPaths = []
 
             for i in range(3, len(slotId)):
-                if int(slotId[i]) < 7:
+                try:
+                    if int(slotId[i]) < 7:
 
-                    targetHelperPath = helperPathData['routeSelections'][(int(slotId[i]) - 1)]
-                    mainPathData['routeSelections'][(int(slotId[i]) - 1)] = targetHelperPath
-                    chosenPaths.append(targetHelperPath['label'])
-                    
-                else:
-                    print(f"{slotId[i]} is not a valid selection.")
-                    continue
+                        targetHelperPath = helperPathData['routeSelections'][(int(slotId[i]) - 1)]
+                        mainPathData['routeSelections'][(int(slotId[i]) - 1)] = targetHelperPath
+                        chosenPaths.append(targetHelperPath['label'])
+
+                    else:
+                        print(f"{slotId[i]} is not a valid selection.")
+                        continue
+
+                except ValueError:
+                    print(f"{slotId[i]} was not valid. Please use numbers 1-6 to select routes.")
+                    break
                 
             else:
                 with open(f"combinedPaths-{timestamp}.json", "w") as combinedPaths:
                     json.dump(mainPathData, combinedPaths, indent = 4)
+                    print(f"'combinedPaths-{timestamp}.json' has been created, and will contain the changes you specified")
+                    print(f"from the following routes:")
+                    for i in chosenPaths:
+                        print("-> " + i)
 
-    print(f"'combinedPaths-{timestamp}.json' has been created, and will contain the changes you specified")
-    print(f"from the following routes:")
-    for i in chosenPaths:
-        print("-> " + i)
     
 if __name__ == "__main__":
     pathMerge(sys.argv[1], sys.argv[2], sys.argv)
